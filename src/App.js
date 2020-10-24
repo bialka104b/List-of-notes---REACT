2f1617080322b7460516a9abf8a58dfbddfdc667
Marta Kulig <bialka104b@interia.pl>
2020-10-24 21:32:37 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    title: "Moja tablica notatek",
+    noteTitle: '',
+    noteText: "",
+    notes: []
+  }
+
+  handleTitle = (e) => {
+    this.setState({
+      noteTitle: e.target.value
+    })
+  }
+  handleText = (e) => {
+    this.setState({
+      noteText: e.target.value
+    })
+  }
+  addNote = () => {
+    let title = this.state.noteTitle;
+
+    let text = this.state.noteText;
+    const newNote = {
+      text: text,
+      title: title,
+      date: new Date(Date.now()).toLocaleString()
+    }
+    this.setState({
+      notes:[...this.state.notes, newNote]
+    })
+  }
+  render(){
+    return (
+      <div className="main-box">
+        <h2 className="main-box__title">{this.state.title}</h2>
+        <div className="main-box__form">
+          <div className="main-box__form-item">
+            <label htmlFor="" className="main-box__form-title">Tytuł notatki</label>
+            <input type="text" className="main-box__form-input" onChange={this.handleTitle} value={this.state.noteTitle}/>
+          </div>
+          <div className="main-box__form-item">
+            <label htmlFor="" className="main-box__form-title"></label>
+            <textarea type="text" className="main-box__form-input" onChange={this.handleText} value={this.state.noteText}></textarea>
+          </div>
+          <div className="main-box__button-box">
+            <button onClick={this.addNote}>Dodaj notatke</button>
+          </div>
+        </div>
+        <div className="main-box__notes-box">
+          <p>Twoje notatki:</p>
+          <div className="notes__box">
+            {
+              this.state.notes.map((note, i)=>
+                <div className="notes__single-note" key={i}>
+                  <p>{note.title}</p>
+                  <p>{note.text}</p>
+                  <p>{note.date}</p>
+                </div>
+              )
+            }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
